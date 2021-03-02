cbf89314b2a9d8bd34c0cb58e57e22797779eaed
marrip <martin.rippin@scilifelab.uu.se>
2021-03-02 12:55:56 +0100

    Feat: Move over to postgres db

--- ddpcr/ddpcr/settings.py
+++ ddpcr/ddpcr/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'ddpcr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ddpcr',
+        'USER': 'postgres',
+        'PASSWORD': 'notsosecret',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
